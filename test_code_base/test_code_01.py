cd9d0de566abee36501e50852416805de5593eb7
feldhausenryan <feldhausenryan@gmail.com>
2016-02-03 12:17:31 -0500

    GETTING STARTED BOYS
    Added Engine.py and then deleted it.
    Added statistician.py which will count the number of words in python
    comments across a directory.
    Added a test code base with two really simple tests.

---
+++ test_code_base/test_code_01.py
@@ -0,0 +1,7 @@
+#This function will print the text "Hello World!" on the command prompt.  
+def print_hello_world():
+    print "Hello World!"
+
+#Test the print_hello_world function
+if __name__ == "__main__":
+    print_hello_world()
