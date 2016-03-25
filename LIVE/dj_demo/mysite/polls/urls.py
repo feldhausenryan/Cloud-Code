f664a9f80b46337b15dac8d689d7736096eefde3
feldhausenryan <feldhausenryan@gmail.com>
2016-03-25 12:01:18 -0400

    Trying to get css to work

--- LIVE/dj_demo/mysite/polls/urls.py
+++ LIVE/dj_demo/mysite/polls/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
 urlpatterns = [url(r'^$', views.index, name='index'),]
+urlpatterns += staticfiles_urlpatterns()
 
 #http://162.243.219.59/
