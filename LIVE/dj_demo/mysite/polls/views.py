def33b34d94f0adb1a3431199e440fdc3c81fc4e
feldhausenryan <feldhausenryan@gmail.com>
2016-03-25 11:39:02 -0400

    linking django to brackets

--- LIVE/dj_demo/mysite/polls/views.py
+++ LIVE/dj_demo/mysite/polls/views.py
@@ -5,4 +5,130 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
+  HTML_RESPONSE = """<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Cloud Code</title>
+    <meta name="description" content="A website searching code.">
+    <link rel="stylesheet" href="static/request.css">
+</head>
+
+<body>
+    <div class=FULL_WINDOW>
+        <div class=FIVE_SPACER>
+
+        </div>
+        <div class=SEARCH_SECTION>
+            <div class=TITLE2>
+                <div style="margin-left:50px;">
+                    Cloud-Code
+                </div>
+
+
+            </div>
+            <div class=SEARCH>
+                <div class=VER_SERACH>
+                    <form>
+                        <input type="search" style="width: 250px;" name="search_box">
+                        <input type="button" value="Go">
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class=GREY_LINE>
+        
+        </div>
+        
+        <div class=BOT_SPACER>
+        </div>
+        
+        <div class=SEARCH_RESULT>
+            <div class=SEARCH_RESULT_TITLE>
+                Return_Spliced_List_From_String() 
+                <a href="http://stackoverflow.com/questions/796087/make-a-div-into-a-link"><span></span></a>
+            </div>
+            <div class=SEARCH_RESULT_DESC>
+                Description of the code above
+            </div>
+        </div>
+        
+        <div class=SEARCH_RESULT>
+            <div class=SEARCH_RESULT_TITLE>
+                Return_Spliced_List_From_String() 
+                <a href="http://stackoverflow.com/questions/796087/make-a-div-into-a-link"><span></span></a>
+            </div>
+            <div class=SEARCH_RESULT_DESC>
+                Description of the code above
+            </div>
+        </div>
+        
+        <div class=SEARCH_RESULT>
+            <div class=SEARCH_RESULT_TITLE>
+                Return_Spliced_List_From_String() 
+                <a href="http://stackoverflow.com/questions/796087/make-a-div-into-a-link"><span></span></a>
+            </div>
+            <div class=SEARCH_RESULT_DESC>
+                Description of the code above
+            </div>
+        </div>
+        
+        <div class=SEARCH_RESULT>
+            <div class=SEARCH_RESULT_TITLE>
+                Return_Spliced_List_From_String() 
+                <a href="http://stackoverflow.com/questions/796087/make-a-div-into-a-link"><span></span></a>
+            </div>
+            <div class=SEARCH_RESULT_DESC>
+                Description of the code above
+            </div>
+        </div>
+        
+        <div class=SEARCH_RESULT>
+            <div class=SEARCH_RESULT_TITLE>
+                Return_Spliced_List_From_String() 
+                <a href="http://stackoverflow.com/questions/796087/make-a-div-into-a-link"><span></span></a>
+            </div>
+            <div class=SEARCH_RESULT_DESC>
+                Description of the code above
+            </div>
+        </div>
+        
+        <div class=SEARCH_RESULT>
+            <div class=SEARCH_RESULT_TITLE>
+                Return_Spliced_List_From_String() 
+                <a href="http://stackoverflow.com/questions/796087/make-a-div-into-a-link"><span></span></a>
+            </div>
+            <div class=SEARCH_RESULT_DESC>
+                Description of the code above
+            </div>
+        </div>
+        
+        <div class=SEARCH_RESULT>
+            <div class=SEARCH_RESULT_TITLE>
+                Return_Spliced_List_From_String() 
+                <a href="http://stackoverflow.com/questions/796087/make-a-div-into-a-link"><span></span></a>
+            </div>
+            <div class=SEARCH_RESULT_DESC>
+                Description of the code above
+            </div>
+        </div>
+        
+        <div class=SEARCH_RESULT>
+            <div class=SEARCH_RESULT_TITLE>
+                Return_Spliced_List_From_String() 
+                <a href="http://stackoverflow.com/questions/796087/make-a-div-into-a-link"><span></span></a>
+            </div>
+            <div class=SEARCH_RESULT_DESC>
+                Description of the code above
+            </div>
+        </div>
+        
+        
+    </div>
+</body>
+
+</html>"""
+  return HttpResponse(HTML_RESPONSE)
